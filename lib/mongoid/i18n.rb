b4aaebbc1a17b080114f7e6bcec783bbd3028228
Thomas Limp <thomas.limp@valiton.com>
2010-08-17 12:14:02 +0200

    added support for criteria scopes

--- lib/mongoid/i18n.rb
+++ lib/mongoid/i18n.rb
@@ -4,21 +4,22 @@ require 'mongoid/i18n/localized_criteria'
 module Mongoid
   module I18n
     extend ActiveSupport::Concern
-    
+
     module ClassMethods
       def localized_field(name, options = {})
         field name, options.merge(:type => LocalizedField)
       end
-      
+
       def criteria
-        I18n::LocalizedCriteria.new(self)
+        scope = scope_stack.last rescue nil
+        scope || I18n::LocalizedCriteria.new(self)
       end
-    
+
       protected
       def create_accessors(name, meth, options = {})
         if options[:type] == LocalizedField
           define_method(meth) { read_attribute(name)[::I18n.locale.to_s] rescue '' }
-          define_method("#{meth}=") do |value| 
+          define_method("#{meth}=") do |value|
             write_attribute(name, (@attributes[name] || {}).merge(::I18n.locale.to_s => value))
           end
           define_method("#{meth}_translations") { read_attribute(name) }
