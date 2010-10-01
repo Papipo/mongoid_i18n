6be5cac2b5c22053b34e952a34380ee847de0bbd
Nicola Racco <nicola.racco@gmail.com>
2010-10-01 09:22:54 +0200

    Added :use_default_if_empty and :clear_blank_values to the localized_field helper

--- lib/mongoid/localizable.rb
+++ lib/mongoid/localizable.rb
@@ -18,17 +18,29 @@ module Mongoid
       protected
       def create_accessors(name, meth, options = {})
         if options[:type] == LocalizedField
-          define_method(meth) { read_attribute(name)[::I18n.locale.to_s] rescue '' }
+          if options[:use_default_if_empty]
+            define_method(meth) do
+              value = read_attribute(name)[::I18n.locale.to_s] rescue ''
+              read_attribute(name)[::I18n.default_locale.to_s] if value.blank? && I18n.locale != I18n.default_locale
+            end
+          else
+            define_method(meth) { read_attribute(name)[::I18n.locale.to_s] rescue '' }
+          end
           define_method("#{meth}=") do |value|
             if value.is_a?(Hash)
               val = (@attributes[name] || {}).merge(value)
             else
               val = (@attributes[name] || {}).merge(::I18n.locale.to_s => value)
             end
+            val.delete_if { |key, value| value.blank? } if options[:clear_empty_values]
             write_attribute(name, val)
           end
           define_method("#{meth}_translations") { read_attribute(name) }
-          define_method("#{meth}_translations=") { |value| write_attribute(name, value) }
+          if options[:clear_empty_values]
+            define_method("#{meth}_translations=") { |value| write_attribute(name, value.delete_if { |key, value| value.blank? }) }
+          else
+            define_method("#{meth}_translations=") { |value| write_attribute(name, value) }
+          end
         else
           super
         end
